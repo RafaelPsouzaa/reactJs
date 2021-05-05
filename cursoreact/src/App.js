8632d6f8c51a84e21b46a2921963b189e56e38ac
rafaelpsouzaa <marksmancicone@gmail.com>
2021-05-05 19:34:07 -0300

    contagem

---
+++ cursoreact/src/App.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+
+function App() {
+  const[contagem,setContagem] = useState(0);
+
+  return (
+    <div >
+      <h1>Contagem:{}</h1>
+    </div>
+  );
+}
+
+export default App;

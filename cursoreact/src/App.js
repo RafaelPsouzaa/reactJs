405b5c27097d6a4b552c3f8d4724d6e66eb5b233
rafaelpsouzaa <marksmancicone@gmail.com>
2021-05-05 21:49:05 -0300

    aula13

--- cursoreact/src/App.js
+++ cursoreact/src/App.js
@@ -2,10 +2,16 @@ import React,{useState,useEffect} from 'react';
 
 function App() {
   const[contagem,setContagem] = useState(0);
+  useEffect(()=>{document.title = ""},[contagem]);
+
+  function aumentarAction(){
+    setContagem(contagem+ 1);
+  }
 
   return (
     <div >
-      <h1>Contagem:{}</h1>
+      <h1>Contagem:{contagem}</h1>
+      <button onClick={aumentarAction}>Aumentar Numero</button>
     </div>
   );
 }

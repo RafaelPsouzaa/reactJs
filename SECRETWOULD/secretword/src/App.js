a2222a92a7bce467e19f17cfa4a21df19c69ddf1
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2025-05-26 08:50:04 -0300

    the end

--- SECRETWOULD/secretword/src/App.js
+++ SECRETWOULD/secretword/src/App.js
@@ -1,161 +1,138 @@
-import { useCallback, useEffect, useState } from "react";
+import './App.css';
+import StartScreen from './components/StartScreen';
+import { useCallback,useState,useEffect } from 'react';
+import { wordsList } from './DATA/words';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
-// components
-import StartScreen from "./components/StartScreen";
-import Game from "./components/Game";
-import GameOver from "./components/GameOver";
-
-// styles
-import "./App.css";
-
-// data
-import { wordsList } from "./data/words";
-
-const stages = [
-  { id: 1, name: "start" },
-  { id: 2, name: "game" },
-  { id: 3, name: "end" },
-];
-
+const stages =[
+  {id:1,name:"start"},
+  {id:2,name:"game"},
+  {id:3,name:"end"},
+]
+const guessesQty = 3;
 function App() {
-  const [gameStage, setGameStage] = useState(stages[0].name);
-  const [words] = useState(wordsList);
+  const [gameStage,setGameStage] = useState(stages[0].name);
+  const [words]=useState(wordsList)
 
-  const [pickedWord, setPickedWord] = useState("");
-  const [pickedCategory, setPickedCategory] = useState("");
-  const [letters, setLetters] = useState([]);
-  
-  const [guessedLetters, setGuessedLetters] = useState([]);
-  const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
-  const [score, setScore] = useState(0);
+  const[pickedWord,setPickedWord] = useState("");
+  const[pickedCategory,setPickedCategory] = useState("");
 
-  console.log(words);
+  const[letters,setLetters] = useState([])
+  const [guessedLetters,setGuessedLetters]=useState([]);
+  const [wrongLetters,setWrongLetters] = useState([]);
+  const[guesses,setGuesses] = useState(guessesQty );
+  const[score,setScore] = useState(50);
 
-  const pickWordAndCategory = useCallback(() => {
-    // pick a random category
+
+ 
+
+  const pickwordAndCategory = useCallback(() => {
+    //Picky a random category
     const categories = Object.keys(words);
-    const category =
-      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const category = categories[Math.floor(Math.random()* Object.keys(categories).length)]
+    console.log(category)
 
-    // pick a random word
-    const word =
-      words[category][Math.floor(Math.random() * words[category].length)];
+    //pick a random word 
+    const word = words[category][Math.floor(Math.random()* words[category].length)]
+    console.log(word)
+    return{word,category}
+  },[words]);
+  
 
-    console.log(category, word);
 
-    return { category, word };
-  }, [words]);
-
-  // start the game
-  const startGame = useCallback(() => {
-    // clear all letters
-    clearLettersStates();
-
-    // choose a word
-    const { category, word } = pickWordAndCategory();
-
-    console.log(category, word);
-
+  //start of game 
+  const startGame = useCallback(()=>{
+    //reset all stages
+      clearLetterStates()
+     
+   const{word,category} = pickwordAndCategory();
+    //create an array of letters
     let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l)=> l.toLowerCase());
 
-    wordLetters = wordLetters.map((l) => l.toLowerCase());
+   console.log(word,category);
+   console.log(wordLetters);
+   //fill states
+   setPickedWord(word);
+   setPickedCategory(category);
+   setLetters(wordLetters);
 
-    // console.log(category, word);
+  },[pickwordAndCategory]);
 
-    setPickedCategory(category);
-    setPickedWord(word);
-    setLetters(wordLetters);
+  //process the letter input 
+  const verifyLetter = (letter) =>{
+   const normalizedLetter =letter.toLowerCase();
 
-    setGameStage(stages[1].name);
-  }, [pickWordAndCategory]);
-
-  // process letter input
-  const verifyLetter = (letter) => {
-    const normalizedLetter = letter.toLowerCase();
-
-    // check if letter has already been utilized
-    if (
-      guessedLetters.includes(normalizedLetter) ||
-      wrongLetters.includes(normalizedLetter)
-    ) {
+    //check if letter has already been utilized
+    if(guessedLetters.includes(normalizedLetter)|| wrongLetters.includes(normalizedLetter)){
       return;
     }
 
-    // push guessed letter or remove a chance
-    if (letters.includes(normalizedLetter)) {
-      setGuessedLetters((actualGuessedLetters) => [
-        ...actualGuessedLetters,
-        letter,
-      ]);
-    } else {
-      setWrongLetters((actualWrongLetters) => [
-        ...actualWrongLetters,
-        normalizedLetter,
-      ]);
+    //push guessed letter or remove a guess
+    if(letters.includes(normalizedLetter)){
+        setGuessedLetters((actualGuessedLetters)=>[
+          ...actualGuessedLetters,normalizedLetter
+        ])
+    }else{
+      setWrongLetters((actualWrongLetters)=>[
+          ...actualWrongLetters,normalizedLetter
+          ])
 
-      setGuesses((actualGuesses) => actualGuesses - 1);
+          setGuesses((actualGuesses)=> actualGuesses -1)
     }
-  };
 
-  console.log(wrongLetters);
+ }
 
-  // restart the game
-  const retry = () => {
-    setScore(0);
-    setGuesses(3);
-    setGameStage(stages[0].name);
-  };
-
-  // clear letters state
-  const clearLettersStates = () => {
+  const clearLetterStates = () => {
     setGuessedLetters([]);
-    setWrongLetters([]);
-  };
-
-  // check if guesses ended
-  useEffect(() => {
-    if (guesses === 0) {
-      // game over and reset all states
-      clearLettersStates();
-
-      setGameStage(stages[2].name);
+    setWrongLetters([])
+  }
+ 
+//check if guesses ended 
+  useEffect(()=>{
+    if(guesses<=0){
+      //reset all stages
+      clearLetterStates()
+      setGameStage(stages[2].name)
     }
-  }, [guesses]);
+  },[guesses])
 
-  // check win condition
-  useEffect(() => {
+  // checked win conditions
+  useEffect(()=>{
     const uniqueLetters = [...new Set(letters)];
 
-    console.log(uniqueLetters);
-    console.log(guessedLetters);
+    //win condition
+    if(guessedLetters.length === uniqueLetters.length){
+      //and score
+      setScore((actualScore) => actualScore += 100)
 
-    // win condition
-    if (guessedLetters.length === uniqueLetters.length) {
-      // add score
-      setScore((actualScore) => (actualScore += 100));
-
-      // restart game with new word
+      //restar game 
       startGame();
+
     }
-  }, [guessedLetters, letters, startGame]);
+
+  },[guessedLetters,letters,startGame])
+
+
+
+  //restart the Game 
+  const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty );
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen startGame={startGame} />}
-      {gameStage === "game" && (
-        <Game
-          verifyLetter={verifyLetter}
-          pickedWord={pickedWord}
-          pickedCategory={pickedCategory}
-          letters={letters}
-          guessedLetters={guessedLetters}
-          wrongLetters={wrongLetters}
-          guesses={guesses}
-          score={score}
-        />
-      )}
-      {gameStage === "end" && <GameOver retry={retry} score={score} />}
+    {gameStage === "start" && <StartScreen startGame={startGame} />}
+    { gameStage === "game" && 
+    <Game verifyLetter={verifyLetter}
+     pickedWord={pickedWord}
+      pickedCategory={pickedCategory} 
+      letters={letters} guessedLetters={guessedLetters} 
+      wrongLetters={wrongLetters}guesses={guesses} score={score}/>}
+    {gameStage === 'end'&& <GameOver retry={retry} score={score}/>}
     </div>
   );
 }
--- SECRETWOULD/secretword/src/components/GameOver.js
+++ SECRETWOULD/secretword/src/components/GameOver.js
@@ -1,6 +1,6 @@
-import "./GameOver.css";
+import '../components/GameOver.css'
 
-const GameOver = ({ retry, score }) => {
+const GameOver = ({retry,score}) => {
   return (
     <div className="gameover">
       <h1>Fim de jogo!</h1>
@@ -12,4 +12,4 @@ const GameOver = ({ retry, score }) => {
   );
 };
 
-export default GameOver;
+export default GameOver
